f64a532ed3a4cabbbafb1de3eab61e4198e60beb
andryue <kacperwojak17@gmail.com>
2020-11-12 17:11:30 +0100

    add custom seekbar

--- Yeebum/app/src/main/java/com/example/yeebum/screens/ColorControlFragment.kt
+++ Yeebum/app/src/main/java/com/example/yeebum/screens/ColorControlFragment.kt
@@ -1,11 +1,13 @@
 package com.example.yeebum.screens
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.yeebum.R
+import kotlinx.android.synthetic.main.fragment_color_control.*
 
 
 class ColorControlFragment : Fragment() {
@@ -18,5 +20,18 @@ class ColorControlFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupColorPicker()
+
+
+
+
+    }
+
+    private fun setupColorPicker(){
+        colorPicker.showOldCenterColor = false
+
+        colorPicker.setOnColorSelectedListener {
+            Log.d("TAG",it.toString())
+        }
     }
 }
