ad329bc8b91d2a8f58514e2c3690f992d4c6f13f
andryue <kacperwojak17@gmail.com>
2020-11-14 09:51:05 +0100

    setup texts to view pager

--- Yeebum/app/src/main/java/com/example/yeebum/screens/ControlFragment.kt
+++ Yeebum/app/src/main/java/com/example/yeebum/screens/ControlFragment.kt
@@ -1,10 +1,14 @@
 package com.example.yeebum.screens
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
+import androidx.viewpager2.widget.ViewPager2
 import com.example.yeebum.R
 import com.example.yeebum.screens.adapters.pager_views.ControlViewPagerAdapter
 import kotlinx.android.synthetic.main.fragment_control.*
@@ -13,13 +17,37 @@ import kotlinx.android.synthetic.main.fragment_control.*
 class ControlFragment : Fragment() {
 
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_control, container, false)
-    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? = inflater.inflate(R.layout.fragment_control, container, false)
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         controlViewPager.adapter = ControlViewPagerAdapter(requireActivity())
+        setupViewPager()
     }
+
+
+    private fun setupViewPager(){
+        val listOfTexts = arrayListOf(colorText, flowsText)
+        listOfTexts.forEach { clickedTV->
+            clickedTV.setOnClickListener {
+                setTextsColor(listOfTexts,listOfTexts.indexOf(clickedTV))
+                controlViewPager.currentItem = listOfTexts.indexOf(clickedTV)
+            }
+        }
+
+        controlViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                setTextsColor(listOfTexts,position)
+                super.onPageSelected(position)
+            }
+        })
+    }
+
+    private fun setTextsColor(listOfTexts:ArrayList<TextView>, position:Int){
+        listOfTexts.forEach { textView -> textView.setTextColor(Color.parseColor("#44FFFFFF")) }
+        listOfTexts[position].setTextColor(Color.parseColor("#FFFFFF"))
+    }
+
 
 }
