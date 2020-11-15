4e15b28758e1cc12cc590736fb2c6b847ae25b87
andryue <kacperwojak17@gmail.com>
2020-11-15 09:53:21 +0100

    create enter flow name screen layout

---
+++ Yeebum/app/src/main/java/com/example/yeebum/screens/EnterActionNameFragment.kt
@@ -0,0 +1,21 @@
+package com.example.yeebum.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.yeebum.R
+
+
+class EnterActionNameFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? = inflater.inflate(R.layout.fragment_enter_action_name, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
