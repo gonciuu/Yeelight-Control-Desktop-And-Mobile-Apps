acca01b3c1d8cfb5ce523db08c1e61ac37dcd519
andryue <kacperwojak17@gmail.com>
2020-11-19 16:09:37 +0100

    setup all drawer with navigation

--- Yeebum/app/src/main/java/com/example/yeebum/MainActivity.kt
+++ Yeebum/app/src/main/java/com/example/yeebum/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.yeebum
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.yeebum.models.DaggerFlowComponent
+import com.example.yeebum.screens.AppDrawer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        AppDrawer().setupDrawerNavigation(this)
 
     }
 }
--- Yeebum/app/src/main/java/com/example/yeebum/screens/AppDrawer.kt
+++ Yeebum/app/src/main/java/com/example/yeebum/screens/AppDrawer.kt
@@ -1,20 +1,75 @@
 package com.example.yeebum.screens
 
 import android.annotation.SuppressLint
+import android.os.Handler
+import android.os.Looper
 import android.view.Gravity
 import android.view.View
+import android.widget.LinearLayout
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.FragmentActivity
+import androidx.navigation.NavOptions
+import androidx.navigation.findNavController
+import com.example.yeebum.MainActivity
 import com.example.yeebum.R
+import kotlinx.android.synthetic.main.drawer.view.*
 
 class AppDrawer {
 
-    @SuppressLint("WrongConstant")
-    fun setOpenDrawer(handler:View, activity:FragmentActivity){
+    //------------------------| Open drawer on whatever button click |------------------------
+    fun setOpenDrawer(handler: View, activity: FragmentActivity) {
         handler.setOnClickListener {
             val drawer = activity.findViewById<DrawerLayout>(R.id.drawer_layout)
-            drawer.openDrawer(Gravity.START)
+            drawer.openDrawer(GravityCompat.START)
         }
     }
+    //=========================================================================================
+
+
+    //----------------------------| Setup drawer with navigation |-------------------------------
+
+    fun setupDrawerNavigation(activity: MainActivity) {
+        val drawer = activity.findViewById<LinearLayout>(R.id.myDrawer)
+        val listOfBoxes = arrayListOf<LinearLayout>(
+            drawer.homeBox,
+            drawer.addBulbBox,
+            drawer.flowsBox,
+            drawer.settingsBox,
+            drawer.helpCenterBox,
+            drawer.bugReportBox
+        )
+        val listOfDestinations = arrayListOf<Int>(
+            R.id.allBulbsFragment,
+            R.id.enterBulbDataFragment,
+            R.id.flowControlFragment,
+            R.id.flowControlFragment,
+            R.id.allBulbsFragment,
+            R.id.enterBulbDataFragment
+        )
+        listOfBoxes.forEach {
+            it.setOnClickListener { _ ->
+                val navController = activity.findNavController(R.id.my_nav_host_fragment)
+                val destination = listOfDestinations[listOfBoxes.indexOf(it)]
+                activity.findViewById<DrawerLayout>(R.id.drawer_layout).closeDrawer(GravityCompat.START)
+                Handler(Looper.getMainLooper()).postDelayed({
+                    if (navController.currentDestination?.id != destination) navController.navigate(
+                        destination,
+                        null,
+                        getNavOptions()
+                    )
+                }, 250) //await to drawer close
+            }
+        }
+    }
+    //===========================================================================================
+
+    //get nav options
+    private fun getNavOptions(): NavOptions = NavOptions.Builder()
+        .setPopExitAnim(R.anim.slide_out_right)
+        .setPopEnterAnim(R.anim.slide_in_left)
+        .setExitAnim(R.anim.slide_out_left)
+        .setEnterAnim(R.anim.slide_in_right)
+        .build()
 
 }
