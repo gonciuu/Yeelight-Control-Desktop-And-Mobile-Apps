93b178e7ed0d86a95ba06d0189d00228ee448f6b
andryue <kacperwojak17@gmail.com>
2020-11-25 17:31:44 +0100

    prepare actions recycler view

--- Yeebum/app/src/main/java/com/example/yeebum/screens/adapters/recycler_views/ActionsRecyclerViewAdapter.kt
+++ Yeebum/app/src/main/java/com/example/yeebum/screens/adapters/recycler_views/ActionsRecyclerViewAdapter.kt
@@ -5,18 +5,41 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.yeebum.R
 import com.example.yeebum.models.Action
+import com.example.yeebum.models.ActionType
 import com.example.yeebum.screens.adapters.recycler_views.view_holders.ActionsViewHolder
 
-class ActionsRecyclerViewAdapter(private val actions:ArrayList<Action>):RecyclerView.Adapter<ActionsViewHolder>() {
+class ActionsRecyclerViewAdapter(private val actions: ArrayList<Action>) :
+    RecyclerView.Adapter<ActionsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActionsViewHolder {
-        return ActionsViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.action_card,parent,false))
+        return ActionsViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.action_card, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: ActionsViewHolder, position: Int) {
+        when (actions[holder.adapterPosition].type) {
+            ActionType.Color -> setupColorAction(holder)
+            ActionType.ColorTemp -> setupColorTempAction(holder)
+            ActionType.Sleep -> setupPauseAction(holder)
+        }
+
 
     }
 
     override fun getItemCount(): Int {
-       return actions.size
+        return actions.size
+    }
+
+
+    private fun setupColorAction(holder: ActionsViewHolder) {
+        //holder.thirdValueImage = actions[holder.adapterPosition].color
+    }
+
+    private fun setupColorTempAction(holder: ActionsViewHolder) {
+
+    }
+
+    private fun setupPauseAction(holder: ActionsViewHolder) {
+
     }
 }
--- Yeebum/app/src/main/java/com/example/yeebum/screens/adapters/recycler_views/view_holders/ActionsViewHolder.kt
+++ Yeebum/app/src/main/java/com/example/yeebum/screens/adapters/recycler_views/view_holders/ActionsViewHolder.kt
@@ -1,6 +1,16 @@
 package com.example.yeebum.screens.adapters.recycler_views.view_holders
 
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.action_card.view.*
 
-class ActionsViewHolder(v:View):RecyclerView.ViewHolder(v)
+class ActionsViewHolder(v:View):RecyclerView.ViewHolder(v){
+    val firstValue: TextView = v.firstValue
+    val firstValueName: TextView = v.firstValueName
+    val secondValue: TextView = v.secondValue
+    val secondValueName: TextView = v.secondValueName
+    val thirdValueImage: ImageView = v.thirdValueImage
+    val thirdValueName: TextView = v.thirdValueName
+}
