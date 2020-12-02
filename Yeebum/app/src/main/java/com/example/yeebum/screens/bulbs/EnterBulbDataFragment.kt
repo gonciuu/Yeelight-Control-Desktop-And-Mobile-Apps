7e39e876e3b0e52a8a069c96ddaa84bc0a2ad53d
andryue <kacperwojak17@gmail.com>
2020-12-02 18:25:20 +0100

    find nearest devices and show it into dialog

--- Yeebum/app/src/main/java/com/example/yeebum/screens/bulbs/EnterBulbDataFragment.kt
+++ Yeebum/app/src/main/java/com/example/yeebum/screens/bulbs/EnterBulbDataFragment.kt
@@ -1,8 +1,14 @@
 package com.example.yeebum.screens.bulbs
 
+import android.app.AlertDialog
+import android.content.Context
 import android.graphics.Color
 import android.graphics.Typeface
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
@@ -11,6 +17,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.yeebum.R
@@ -22,10 +29,53 @@ import com.example.yeebum.screens.components.LoadingDialog
 import kotlinx.android.synthetic.main.fragment_enter_bulb_data.*
 import kotlinx.coroutines.delay
 import java.lang.Exception
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
 
 
 class EnterBulbDataFragment : Fragment() {
 
+    companion object{
+        const val MSG_SHOWLOG = 0
+        const val MSG_FOUND_DEVICE = 1
+        const val MSG_DISCOVER_FINISH = 2
+        const val MSG_STOP_SEARCH = 3
+        const val UDP_HOST = "239.255.255.250"
+        const val UDP_PORT = 1982
+        const val message: String = "M-SEARCH * HTTP/1.1\r\n" +
+                "HOST:239.255.255.250:1982\r\n" +
+                "MAN:\"ssdp:discover\"\r\n" +
+                "ST:wifi_bulb\r\n"
+    }
+
+    private var loadingDialog :AlertDialog? = null
+    private val listOfDevices = ArrayList<HashMap<String,String>>()
+    private lateinit var multicastLock: WifiManager.MulticastLock
+    private lateinit var searchThread:Thread
+    private var isSearching = true
+    private val mHandler: Handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            when (msg.what) {
+                MSG_FOUND_DEVICE -> {
+                    loadingDialog?.dismiss()
+                    Helpers().getDevicesDialog(requireActivity(),requireContext(),"Choose Device",listOfDevices)
+                }
+                MSG_SHOWLOG -> Toast.makeText(requireContext(), "" + msg.obj.toString(), Toast.LENGTH_SHORT).show()
+                MSG_STOP_SEARCH -> {
+                    loadingDialog?.dismiss()
+                    searchThread.interrupt()
+                    Helpers().getDevicesDialog(requireActivity(),requireContext(),"Choose Device",listOfDevices)
+                    isSearching = false
+                }
+                MSG_DISCOVER_FINISH -> {
+                    loadingDialog?.dismiss()
+                    Helpers().getDevicesDialog(requireActivity(),requireContext(),"Choose Device",listOfDevices)
+                }
+            }
+        }
+    }
     private val bulbsViewModel: BulbsViewModel by viewModels {
         BulbsViewModelFactory((requireActivity().application as YeebumApplication).bulbsRepository)
     }
@@ -37,8 +87,11 @@ class EnterBulbDataFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        loadingDialog = LoadingDialog.getDialog(requireContext(),"Searching For Devices...")
 
-
+        val wm = requireActivity().applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        multicastLock = wm.createMulticastLock("yeebum")
+        multicastLock.acquire()
         searchForDevices()
 
         setSpannableText()
@@ -91,19 +144,62 @@ class EnterBulbDataFragment : Fragment() {
 
 
     private fun searchForDevices(){
-        val loadingDialog = LoadingDialog.getDialog(requireContext(),"Searching For Devices...")
-        loadingDialog.show()
+
+        loadingDialog?.show()
+
+        listOfDevices.clear()
+        searchThread = Thread {
+            val mDSocket = DatagramSocket()
+            val dpSend = DatagramPacket(message.toByteArray(), message.toByteArray().size, InetAddress.getByName(UDP_HOST), UDP_PORT)
+            mDSocket.send(dpSend)
+            mHandler.sendEmptyMessageDelayed(MSG_STOP_SEARCH, 2000)
+            while (isSearching){
+                val buf = ByteArray(1024)
+                val dpReceive = DatagramPacket(buf, buf.size)
+                mDSocket.receive(dpReceive)
+                val bytes = dpReceive.data
+                val buffer = StringBuffer()
+                for(i in 0 until dpReceive.length){
+                    if(bytes[i] == 13.toByte()){
+                        continue
+                    }
+                    buffer.append(bytes[i].toChar())
+                }
+
+                if(!buffer.toString().contains("yeelight")){
+                    mHandler.obtainMessage(MSG_SHOWLOG, "XD").sendToTarget()
+                    return@Thread
+                }
+
+                val infos = buffer.toString().split("\n")
+                val bulbInfoHashMap = HashMap<String, String>()
+                for(i in infos) {
+                    val index = i.indexOf(":")
+                    if(index==-1) continue
+                    val title = i.substring(0, index)
+                    val value = i.substring(index + 1)
+                    bulbInfoHashMap.put(title, value)
+                }
+                if(!hasAdd(bulbInfoHashMap)){
+                    listOfDevices.add(bulbInfoHashMap)
+                }
+            }
+            mHandler.sendEmptyMessage(MSG_DISCOVER_FINISH)
+        }
+        searchThread.start()
 
 
-
-        val list = ArrayList<HashMap<String,String>>()
-        list.add(hashMapOf("model" to "XDD","Location" to "XDDDD"))
-        //Helpers().getDevicesDialog(requireActivity(),requireContext(),"Choose Device",list)
-
-        //loadingDialog.dismiss()
     }
 
-
+    
+    private fun hasAdd(bulbInfo: HashMap<String, String>): Boolean {
+        listOfDevices.forEach {
+            if (it["Location"].equals(bulbInfo["Location"])) {
+                return true
+            }
+        }
+        return false
+    }
 
 
 }
